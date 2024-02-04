6ed419726a3dd03982d0034fdf47d65e1472417c
BAE BYEONGIL <b000202@naver.com>
2024-02-05 02:53:07 +0900

    react_Learn

--- src/App.js
+++ src/App.js
@@ -2,15 +2,13 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
+  let post = "신림 국밥 맛집";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>react learn !!</p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <h4>This is blog</h4>
+      </div>
+      <h4>{post}</h4>
     </div>
   );
 }
